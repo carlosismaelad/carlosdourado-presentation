9aaf414f0867d7c425a365e028f38a5e877eef4c
Carlos Dourado <carlosismaelad@outlook.com>
2025-01-06 17:14:56 -0300

    rename component

---
+++ src/components/sections/PresentationText.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="presentation">
+    <h1>
+      {{ presentation.initText }}
+      <br />
+      <span ref="typedElement" class="tech">{{ currentWord }}</span>
+      <span>{{ presentation.finalText }}</span>
+    </h1>
+  </div>
+</template>
+
+<script>
+import Typewriter from "typewriter-effect/dist/core";
+import { presentation } from "../../util/texts/en/presentation";
+export default {
+  name: "PresentationText",
+  data() {
+    return {
+      presentation,
+    };
+  },
+  mounted() {
+    const options = {
+      strings: [
+        "Java",
+        "Node.js",
+        "C#",
+        "Javascript",
+        "Vue.js",
+        "React",
+        "Back end",
+        "Front end",
+      ],
+      autoStart: true,
+      loop: true,
+      cursor: "|",
+      delay: 75,
+      deleteSpeed: 40,
+    };
+
+    new Typewriter(this.$refs.typedElement, options);
+  },
+};
+</script>
+
+<style>
+.presentation {
+  display: flex;
+  margin: 5rem auto;
+  color: beige;
+  place-content: center;
+  font-family: "Roboto Mono", sans-serif;
+  font-weight: 600;
+  font-size: 1.5rem;
+  text-wrap: wrap;
+}
+
+.tech {
+  color: var(--primary-color);
+  margin-top: 1rem;
+  display: inline-block;
+}
+</style>
