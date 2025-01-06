82a5c8f094f16778db58e4552e294f7e465037f8
Carlos Dourado <carlosismaelad@outlook.com>
2025-01-06 17:16:09 -0300

    Add types and util folder

---
+++ src/types/texts.ts
@@ -0,0 +1,4 @@
+export interface Presentation {
+  initText: string;
+  finalText: string;
+}
---
+++ src/util/texts/en/presentation.ts
@@ -0,0 +1,6 @@
+import type { Presentation } from "@/types/texts";
+
+export const presentation: Presentation = {
+  initText: "Hello! My name is Carlos and i'm a",
+  finalText: "developer!",
+};
